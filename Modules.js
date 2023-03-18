cdde4e559686f967e3b0366b92c952de12a6c1ae
Ashureth <126100460+Ashureth@users.noreply.github.com>
2023-03-18 16:51:12 +0100

    Update Modules.js

--- Modules.js
+++ Modules.js
@@ -5,13 +5,7 @@ class MooMoo {
     this.fetchSocket = function() {
       WebSocket.prototype.send = new Proxy(WebSocket.prototype.send, {
         apply(target, socket, data) {
-          WebSocket.prototype.send = target;
-          
-          if(this.socket === null || this.socket.url != socket.url || this.socket.readyState > 1) {
-              this.socket = socket;
-              console.log('Hooked', this.socket)
-          }
-          
+          this.socket = socket;
           Reflect.apply(target, socket, data);
         }
       });
